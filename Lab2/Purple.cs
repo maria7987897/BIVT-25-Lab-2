c717646cd6bf7437f7f13a1fc8f4e3d0cf87ede8
korneyevvitya-debug <korneyev.vitya@gmail.com>
2025-09-09 16:08:23 +0300

    Update Purple.cs

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Lab2
 {
@@ -12,7 +13,10 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            for (int i = 0; i<n; i++)
+            {
+                answer += (p + h * i) * (p + h * i);
+            }
             // end
 
             return answer;
@@ -23,7 +27,12 @@ namespace Lab2
             int remainder = 0;
 
             // code here
-
+            while (a>=b)
+            {
+                a -= b;
+                quotient++;
+            }
+            remainder = a;
             // end
 
             return (quotient, remainder);
@@ -33,7 +42,15 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            double fi1 = 1.0, fi2 = 1.0, fi3 = 2.0, fi4;
+            while (Math.Abs((double)fi3/fi2 - (double)fi2/fi1) > 0.0001)
+            {
+                fi4 = fi2 + fi3;
+                fi1 = fi2;
+                fi2 = fi3;
+                fi3 = fi4;
+            }
+            answer = (double)fi3 / (double)fi2;
             // end
 
             return answer;
@@ -43,7 +60,12 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            answer++;
+            while (Math.Abs(b) > 0.0001)
+            {
+                answer++;
+                b *= q;
+            }
             // end
 
             return answer;
@@ -53,7 +75,17 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            long number = a;
+            while (b > 0)
+            {
+                number *= b;
+                b--;
+            }
+            while ( number >= 10)
+            {
+                number /= 10;
+                answer++;
+            }
             // end
 
             return answer;
@@ -63,7 +95,7 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            answer = (long)(~(ulong)0/ 15000000);
             // end
 
             return answer;
@@ -74,18 +106,60 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            double money = 0, Stemp=S;
+            while (Stemp < S*2)
+            {
+                if (answer % 12 == 0)
+                {
+                    money = Stemp * d / 1200.0;
+                }
+                answer++;
+                Stemp += money;
+            }
             // end
 
             return answer;
         }
         public (double SS, double SY) Task8(double a, double b, double h)
         {
+            static long Fact(int n)
+            {
+                if (n == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return n * Fact(n - 1);
+                }
+            }
+            static double GetPow(double n, int i)
+            {
+                if (i == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return n * GetPow(n, i - 1);
+                }
+            }
             double SS = 0;
             double SY = 0;
-
+            int i = 1;
+            double temp = 0.5;
             // code here
-
+            for (double x = a; x <= b; x += h)
+            {
+                i = 1;
+                SY += Math.Cos(x);
+                temp = 0.5;
+                while (Math.Abs(temp) >= 0.0001)
+                {
+                    SS += temp;
+                    temp = GetPow(-1, i) * GetPow(x, 2 * i) / Fact(2 * i);
+                }
+            }
             // end
 
             return (SS, SY);
