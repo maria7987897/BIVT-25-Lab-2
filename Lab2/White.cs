a69317ee2bedbbdd25677b7489c30074954cb405
TarkovskayaAD <amerika.fort@mail.ru>
2025-09-12 15:56:34 +0300

    Update White.cs 1-5, 7

--- Lab2/White.cs
+++ Lab2/White.cs
@@ -1,4 +1,4 @@
-﻿namespace Lab2
+namespace Lab2
 {
     public class White
     {
@@ -8,7 +8,10 @@
             int answer = 0;
 
             // code here
-
+            for (int i = 1; i <= n; i++)
+            {
+                answer += (3 * i - 1);
+            }
             // end
 
             return answer;
@@ -18,7 +21,10 @@
             double answer = 0;
 
             // code here
-
+            for (double i = 1; i <= n; i++)
+            {
+                answer += (1 / i);
+            }
             // end
 
             return answer;
@@ -28,7 +34,11 @@
             long answer = 0;
 
             // code here
-
+            answer += 1;
+            for (int i = 1; i <= n; i++)
+            {
+                answer *= i;
+            }
             // end
 
             return answer;
@@ -38,7 +48,19 @@
             long answer = 0;
 
             // code here
-
+            long s = a;
+            if (b == 0)
+            {
+                answer = 1;
+            }
+            else
+            {
+              for (long i = 1; i < b; i++)
+                {
+                    s *= a;
+                }
+            answer = s;  
+            }               
             // end
 
             return answer;
@@ -48,7 +70,14 @@
             int answer = 0;
 
             // code here
-
+            int p = 1;
+            int n1 = 0;
+            for (int n = 1; p <= L; n += 3)
+            {
+                p *= n;
+                n1 = n;
+            }
+            answer = n1;
             // end
 
             return answer;
@@ -56,10 +85,13 @@
         public double Task6(double x)
         {
             double answer = 0;
-            const double R = 6371.0; // радиус Земли, км
+
 
             // code here
-
+            for (int i = 0; Math.Pow(x, i * 2) < 0.0001; i++)
+            {
+                answer += Math.Pow(x, i*2);
+            }
             // end
 
             return answer;
@@ -70,7 +102,11 @@
             int answer = 0;
 
             // code here
-
+            int sum = 0;
+            while (sum < n) {
+                answer++;
+                sum += answer;
+            }
             // end
 
             return answer;
@@ -78,6 +114,7 @@
         public int Task8(double L, double v)
         {
             int answer = 0;
+            const double R = 6371.0; // радиус Земли, км
 
             // code here
 
