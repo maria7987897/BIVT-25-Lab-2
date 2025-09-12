3f988f5bbebed6b68a1b89798a726d2fd8778097
triplesixblast <128070058+triplesixblast@users.noreply.github.com>
2025-09-13 02:07:56 +0300

    Update White.cs

--- Lab2/White.cs
+++ Lab2/White.cs
@@ -2,13 +2,16 @@
 {
     public class White
     {
-        const double E = 0.0001;
         public int Task1(int n)
         {
             int answer = 0;
 
             // code here
-
+            if (n == 0) answer = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                answer += 3 * i - 1;
+            }
             // end
 
             return answer;
@@ -18,7 +21,11 @@
             double answer = 0;
 
             // code here
-
+            if (n == 0) answer = 0;
+            for (double i = 1; i <= n; i++)
+            {
+                answer += 1 / i;
+            }
             // end
 
             return answer;
@@ -28,7 +35,12 @@
             long answer = 0;
 
             // code here
-
+            answer += 1;
+            if (n == 0 || n == 1) answer = 1;
+            for (long i = 2; i <= n; i++)
+            {
+                answer *= i;
+            }
             // end
 
             return answer;
@@ -38,7 +50,13 @@
             long answer = 0;
 
             // code here
-
+            answer += 1;
+            if (a == 0) answer = 0;
+            if (b == 0) answer = 1;
+            for (int i = 0; i < b; i++)
+            {
+                answer *= a;
+            }
             // end
 
             return answer;
@@ -48,7 +66,14 @@
             int answer = 0;
 
             // code here
-
+            answer += 1;
+            int p = 1;
+            if (L < 1) answer = 1;
+            while (p <= L)
+            {
+                answer += 3;
+                p *= answer;
+            }
             // end
 
             return answer;
@@ -56,10 +81,16 @@
         public double Task6(double x)
         {
             double answer = 0;
-            const double R = 6371.0; // радиус Земли, км
+            const double E = 0.0001;
 
             // code here
-
+            if (Math.Abs(x) > 1) answer = 1;
+            double s = 1;
+            while (s >= E)
+            {
+                answer += s;
+                s *= x * x;
+            }
             // end
 
             return answer;
@@ -70,7 +101,12 @@
             int answer = 0;
 
             // code here
-
+            int sum = 0;
+            while (sum < n)
+            {
+                answer++;
+                sum += answer;
+            }
             // end
 
             return answer;
@@ -78,9 +114,11 @@
         public int Task8(double L, double v)
         {
             int answer = 0;
+            const double R = 6371.0; // радиус Земли, км
 
             // code here
-
+            double h = (Math.Sqrt(R * R + L * L) - R);
+            answer = (int)Math.Ceiling(h / v);
             // end
 
             return answer;
