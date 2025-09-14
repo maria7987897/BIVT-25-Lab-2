ae56783365735109eeeb0f852706b953a863e9e0
Daniel <007danlin@gmail.com>
2025-09-14 09:46:22 +0300

    Green and White

--- Lab2/Green.cs
+++ Lab2/Green.cs
@@ -6,62 +6,109 @@ namespace Lab2
     {
         const double E = 0.0001;
         const double Da = 0.0000000001;
+
         public double Task1(int n)
         {
             double answer = 0;
 
             // code here
-
+            for (int i = 2; i <= n; i += 2)
+            {
+                answer += (double)i / (i + 1);
+            }
             // end
 
             return answer;
         }
+
         public double Task2(int n, double x)
         {
             double answer = 0;
 
             // code here
-
+            for (int i = 0; i <= n; i++)
+            {
+                answer += Math.Pow(x, -i);
+            }
             // end
 
             return answer;
         }
+
         public long Task3(int n)
         {
             long answer = 0;
 
             // code here
+            long factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+                answer += factorial;
+            }
 
+            answer += 1;
             // end
 
             return answer;
         }
+
         public double Task4(double x)
         {
             double answer = 0;
 
             // code here
-
+            int i = 0;
+            double number = 1;
+            double epsilon = 0.0001;
+            while (Math.Abs(number) >= epsilon)
+            {
+                i++;
+                number = Math.Sin(i * (Math.Pow(x, i)));
+                answer += number;
+            }
             // end
 
             return answer;
         }
+
         public int Task5(double x)
         {
-            int answer = 0;
-
             // code here
+            int answer = 0;
+            double epsilon = 0.0001;
+            int n = 0;
+            while (true)
+            {
+                double left = (double)1 / Math.Pow(x, n);
+                double right = (double)1 / Math.Pow(x, n - 1);
+                if (Math.Abs((left) - (right)) < epsilon)
+                {
+                    break;
+                }
 
+                n++;
+            }
+
+            answer = n;
             // end
 
             return answer;
         }
+
         public int Task6(int limit)
         {
             int answer = 0;
 
             // code here
-
+            int elem = 1;
+            int i = 0;
+            while (elem < limit)
+            {
+                elem *= 2;
+                answer += elem;
+                i++;
+            }
             // end
 
             return answer;
@@ -70,20 +117,37 @@ namespace Lab2
         public int Task7(double L)
         {
             int answer = 0;
-
             // code here
-
+            while (L > Da)
+            {
+                L /= 2;
+                answer += 1;
+            }
             // end
 
             return answer;
         }
-        public (double SS, double SY) Task8(double a, double b, double h)
+    
+
+    public (double SS, double SY) Task8(double a, double b, double h)
         {
             double SS = 0;
             double SY = 0;
 
             // code here
-
+            for (double x = a; x <= b; x += h)
+            {
+                SY += Math.Atan(x);
+                double item = 0;
+                int i = 0;
+                do
+                {
+                    item = Math.Pow(-1, i) * Math.Pow(x, 2 * i + 1) / (2 * i + 1);
+                    i++;
+                    SS += item;
+                } while (Math.Abs(item)>=E);
+            }
+            
             // end
 
             return (SS, SY);
--- Lab2/White.cs
+++ Lab2/White.cs
@@ -3,32 +3,45 @@
     public class White
     {
         const double E = 0.0001;
+
         public int Task1(int n)
         {
             int answer = 0;
 
             // code here
-
+            for (int i = 1; i <= n; i++)
+            {
+                answer += 3 * i - 1;
+            }
             // end
 
             return answer;
         }
+
         public double Task2(int n)
         {
             double answer = 0;
 
             // code here
-
+            for (int i = 1; i <= n; i++)
+            {
+                answer += (double)1 / i;
+            }
             // end
 
             return answer;
         }
+
         public long Task3(int n)
         {
             long answer = 0;
 
             // code here
-
+            answer = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                answer *= i;
+            }
             // end
 
             return answer;
@@ -38,7 +51,11 @@
             long answer = 0;
 
             // code here
-
+            answer = 1;
+            for (int i = 1; i <= b; i++)
+            {
+                answer *= a;
+            }
             // end
 
             return answer;
@@ -48,18 +65,30 @@
             int answer = 0;
 
             // code here
-
+            int p = 1;
+            int n = 1;
+            while (p <= L)
+            {
+                p *= n;
+                n += 3;
+            }
+            answer = n - 3;
             // end
 
             return answer;
         }
+
         public double Task6(double x)
         {
             double answer = 0;
-            const double R = 6371.0; // радиус Земли, км
-
             // code here
-
+            answer = 1;
+            int n = 1;
+            while (Math.Pow(x, 2 * n) >= E)
+            {
+                answer += Math.Pow(x, 2 * n);
+                n += 1;
+            } 
             // end
 
             return answer;
@@ -70,17 +99,34 @@
             int answer = 0;
 
             // code here
-
+            int sum = 0;
+            while (sum < n)
+            {
+                answer++;
+                sum += answer;
+            }
             // end
 
             return answer;
         }
+
         public int Task8(double L, double v)
         {
+            const double R = 6371.0; // радиус Земли, км
             int answer = 0;
 
             // code here
+            double height = 0;
+            int hours = 0;
+            double distance = 0;
+            while (distance <= L)
+            {
+                hours++; //смотрим, сколько часов
+                height = v * hours; //считаем высоту, на которую поднялся за каждый час
+                distance = Math.Sqrt(2 * R * height + Math.Pow(height, 2)); //вычисляем дистанцию/расстояние
+            }
 
+            answer = hours;
             // end
 
             return answer;
