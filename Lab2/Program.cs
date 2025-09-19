0d9868643c3d7a7f83520fb4ce9c14784ed75852
Liner <133678221+Liner11@users.noreply.github.com>
2025-09-20 00:34:39 +0300

    Update Program.cs

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -8,8 +8,12 @@ namespace Lab2
 
             Console.WriteLine(blue.Task1(15, -1.42));
             Console.WriteLine(blue.Task2(10));
-            Console.WriteLine(blue.Task3(11));
-            Console.WriteLine(blue.Task4(3, 30, 100));
+            Console.WriteLine(blue.Task3(3));
+            Console.WriteLine(blue.Task4(0, 1, 10));
+            Console.WriteLine(blue.Task5(1.1));
+            Console.WriteLine(blue.Task6(1, 1, 100));
+            Console.WriteLine(blue.Task7(1, 0.1));
+            Console.WriteLine(blue.Task8(1, 0.1, 0));
         }
     }
 }
