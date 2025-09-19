58f31efd9571af5dd86b5c0a3469896e99d59ad2
Liner <133678221+Liner11@users.noreply.github.com>
2025-09-19 20:34:23 +0300

    Update Program.cs

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Lab2
+namespace Lab2
 {
     public class Program
     {
         public static void Main()
         {
+            Blue blue = new Blue();
 
+            Console.WriteLine(blue.Task1(15, -1.42));
+            Console.WriteLine(blue.Task2(10));
+            Console.WriteLine(blue.Task3(11));
+            Console.WriteLine(blue.Task4(3, 30, 100));
         }
     }
 }
