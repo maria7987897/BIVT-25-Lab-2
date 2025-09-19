d85f23241ea3eeaf1e4fea72942c2ff25b3497b8
MarinArtemiy <m2512392@edu.misis.ru>
2025-09-19 10:40:24 +0300

    Add files via upload

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -12,7 +12,14 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            
+            for (int i=1; i<=n; i++)
+            {
+                int a = (p + h*(i - 1) );
+                answer += a * a;
+                Console.WriteLine(answer);
+            }
+            Console.WriteLine(answer);
             // end
 
             return answer;
@@ -23,7 +30,12 @@ namespace Lab2
             int remainder = 0;
 
             // code here
-
+            while (a >= b)
+            {
+                a = a - b;
+                quotient++;
+            }
+            remainder = a;
             // end
 
             return (quotient, remainder);
@@ -33,7 +45,23 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            // a/b c/d a+c/b+d
+            // 1/1 2/1 3/2
+            double a1=1,b1=1,a2=2,b2=1,a3=3,b3=2,a4=0,b4=1;
+            while (true)
+            {
+                if (Math.Abs((a3 / b3) - (a4 / b4)) < E)
+                {
+                    answer = a4 / b4;
+                    break;
+                }
+                a4 = a2 + a3; b4 = b2 + b3; //calculating num4 parts
 
+                a1 = a2; b1 = b2; //num2->num1, forget num1
+                a2 = a3; b2 = b3; //num3->num2
+                a3 = a4; b3 = b4; //num4->num3
+
+            }
             // end
 
             return answer;
