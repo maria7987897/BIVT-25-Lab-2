d0dc11ada3ed2e55e399a9a68f03919aef4b7829
MarinArtemiy <m2512392@edu.misis.ru>
2025-09-20 17:39:14 +0300

    Add files via upload

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 namespace Lab2
 {
@@ -50,12 +53,13 @@ namespace Lab2
             double a1=1,b1=1,a2=2,b2=1,a3=3,b3=2,a4=0,b4=1;
             while (true)
             {
+                a4 = a2 + a3; b4 = b2 + b3; //calculating num4 parts
+
                 if (Math.Abs((a3 / b3) - (a4 / b4)) < E)
                 {
                     answer = a4 / b4;
                     break;
                 }
-                a4 = a2 + a3; b4 = b2 + b3; //calculating num4 parts
 
                 a1 = a2; b1 = b2; //num2->num1, forget num1
                 a2 = a3; b2 = b3; //num3->num2
@@ -71,7 +75,16 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            while (true)
+            {
+                answer++;
+                if (Math.Abs(b) < E)
+                {
+                    break;
+                }
+                b *= q;
+                //num = num * q
+            }
             // end
 
             return answer;
@@ -81,7 +94,19 @@ namespace Lab2
             int answer = 0;
 
             // code here
+            long number = a;
 
+            while (b > 0)
+            {
+                number *= b;
+                b--;
+            }
+
+            while (number >= 10)
+            {
+                number /= 10;
+                answer++;
+            }
             // end
 
             return answer;
@@ -91,7 +116,14 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            ulong a = 0;
+            ulong count = 1;
+            for (int i = 1; i <= 64; i++)
+            {
+                a += count;
+                count *= 2;
+            }
+            answer = (long)(a / 15_000_000);
             // end
 
             return answer;
@@ -99,24 +131,68 @@ namespace Lab2
 
         public int Task7(double S, double d)
         {
-            int answer = 0;
+            int answer = 0; //doubles as month counter
 
             // code here
-
+            double d_monthly = d / 1200;
+            double double_S = 2 * S;
+            double year_start_S = 0;
+            while (S < double_S)
+            {
+                if (answer % 12 == 0)
+                {
+                    year_start_S = S;
+                }
+                    S = S + year_start_S * d_monthly;
+                answer++;
+            }
             // end
 
             return answer;
         }
         public (double SS, double SY) Task8(double a, double b, double h)
+{
+    const double E = 0.0001;
+    double SS = 0;
+    double SY = 0;
+    //double x = 0;
+    // code here
+    for (decimal x = (decimal)a; x <= (decimal)b; x += (decimal)h) // x in [a,b]
+    {
+        decimal s_sum = 0; //row
+        decimal s = 1; //row member
+        int i = 0;
+        while (Math.Abs(s) >= (decimal)E) //sum of rows for x
         {
-            double SS = 0;
-            double SY = 0;
 
-            // code here
+            //s1 (-1*x*x)^i
+            decimal s1 = 1;
+            for (int i1 = 1; i1 <= i; i1++)
+            {
+                s1 *= (-1 * x * x);
+            }
 
-            // end
+            decimal s2 = 1;
+            //s2 (2i)!
+            for (int i2 = 1; i2 <= 2 * i; i2++)
+            {
+                s2 *= i2;
+            }
+
+            s = (s1 / s2);  //s == s1/s2
+            s_sum += s;
+            i++;
+        }
+        SS += (double)s_sum; //adding row to sum of S
+
+        double y = Math.Cos((double)x);
+        SY += y; //addinf cos(x) to sum of Y
+    }
+        // SS=Math.Round(SS, 6);
+        // SY=Math.Round(SY, 6);
+    // end
 
             return (SS, SY);
-        }
+}
     }
 }
