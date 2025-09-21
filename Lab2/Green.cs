9ac8500b7b3ec1dee36e40db093878bf8b7991cd
nikita-kholev <nikitakholev73@yandex.ru>
2025-09-21 15:18:47 +0300

    PURPLE and GREEN

--- Lab2/Green.cs
+++ Lab2/Green.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Xml;
 
 namespace Lab2
 {
@@ -11,7 +12,10 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            for (double i = 2; i <= n; i+= 2)
+            {
+                answer += i / (i + 1);
+            }
             // end
 
             return answer;
@@ -21,7 +25,19 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            if (x == 0)
+            {
+                return answer;
+            }
+            for (long i = 0; i <= n; i++)
+            {
+                double f = 1;
+                for (long j = 1; j <= i; j++)
+                {
+                    f /= x;
+                }
+                answer += f;
+            }
             // end
 
             return answer;
@@ -31,7 +47,15 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
+            for (int i = 0; i <= n; i++)
+            {
+                long f = 1;
+                for (long j = 1; j <= i; j++)
+                {
+                    f *= j;
+                }
+                answer += f;
+            }
             // end
 
             return answer;
@@ -41,7 +65,16 @@ namespace Lab2
             double answer = 0;
 
             // code here
-
+            double temp = 1;
+            double v = Math.Sin(temp * x);
+            double multipyer = x;
+            while (Math.Abs(v) >= E)
+            {
+                answer += v;
+                x *= multipyer;
+                temp += 1;
+                v = Math.Sin(temp * x);
+            }
             // end
 
             return answer;
@@ -51,7 +84,15 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            double almost_last = 1;
+            double last = 1 / x;
+            answer++;
+            while (Math.Abs(last - almost_last) >= E)
+            {
+                answer++;
+                last /= x;
+                almost_last /= x;
+            }
             // end
 
             return answer;
@@ -61,7 +102,14 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            int elem = 1;
+            int i = 0;
+            while (elem < limit)
+            {
+                elem *= 2;
+                answer += elem;
+                i++;
+            }
             // end
 
             return answer;
@@ -72,7 +120,11 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            while (L > E * E * E * 100)
+            {
+                answer++;
+                L /= 2;
+            }
             // end
 
             return answer;
@@ -83,7 +135,26 @@ namespace Lab2
             double SY = 0;
 
             // code here
-
+            for (decimal x = (decimal) a; x <= (decimal) b; x += (decimal) h)
+            {
+                decimal v = 1;
+                int i = 1, power = 1;
+                SS += (double) x;
+                while ((double)Math.Abs(v) >= E)
+                {
+                    i = -i;
+                    decimal znam = 2 * power + 1;
+                    decimal chi = 1;
+                    for (int j = 1; j <= 2 * power + 1; j++)
+                    {
+                        chi *= x;
+                    }
+                    v = i * chi / znam;
+                    SS += (double)v;
+                    power += 1;
+                }
+                SY += Math.Atan((double)x);
+            }
             // end
 
             return (SS, SY);
