897f68f3abfc7f5f33d33f3d4666898daf0a3eab
CVVAROG <163721519+CVVAROG@users.noreply.github.com>
2025-09-23 12:52:13 +0300

    Update Green.cs

--- Lab2/Green.cs
+++ Lab2/Green.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 namespace Lab2
 {
@@ -11,6 +12,19 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            for (double i = 2; i <= n && n>2; i+=2)
+            {
+                
+                answer += i / (i + 1);
+            }
+            if( n <= 2)
+            {
+                answer = (double)2 / (double)3;
+            }
+            if (n == 0)
+            {
+                answer = 0;
+            }
 
             // end
 
@@ -21,6 +35,12 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            double a = 1;
+            for(int i = 0; i <= n; i++)
+            {
+                answer += a ;
+                a = a / x;
+            }
 
             // end
 
@@ -31,7 +51,30 @@ namespace Lab2
             long answer = 0;
 
             // code here
+            int a = 1;
+            int b = 0;
+            long s = 0;
+            int i = 0;
+            while (i <= n)
+            {
+                if (i == 0)
+                {
+                    s += 1;
 
+                }
+                else
+                {
+                    s =s* a;
+                    a++;
+
+
+                }
+                i++;
+                answer += s;
+            }
+
+
+            
             // end
 
             return answer;
@@ -41,6 +84,23 @@ namespace Lab2
             double answer = 0;
 
             // code here
+            int n = 100000;
+            int a = 1;
+            int i = 1;
+            double k = x;
+            while (i <= n)
+            {
+                answer += Math.Sin(a * k);
+                double r =Math.Abs( Math.Sin(a * k));
+                a++;
+                k = k * x;
+                if (r < 0.0001)
+                {
+                    break;
+                }
+                i++;
+
+            }
 
             // end
 
@@ -51,7 +111,25 @@ namespace Lab2
             int answer = 0;
 
             // code here
+            int n = 100000;
+            int i = 1;
+            double k = x;
+            double nowx = 0;
+            double lastx = 11111;
+            while (i <= n)
+            {
+                nowx = 1 / k;
+                k = k * x;
+                if (Math.Abs(nowx - lastx) < 0.0001)
+                {
+                    answer = i;
+                    break;
+                }
+                lastx = nowx;
+                i++;
 
+
+            }
             // end
 
             return answer;
@@ -61,6 +139,15 @@ namespace Lab2
             int answer = 0;
 
             // code here
+            int i = 0;
+            int elem = 1;
+            while (elem < limit) 
+            {
+                elem *= 2;
+                answer += elem;
+                i++;
+                
+            }
 
             // end
 
@@ -72,6 +159,23 @@ namespace Lab2
             int answer = 0;
 
             // code here
+            int i = 1;
+            int n = 10000000;
+            while (i != n)
+            {
+                L = L / 2;
+                answer++;
+                if (L == 0)
+                {
+                    answer = 0;
+                    break;
+                }
+                if (L <= Da)
+                {
+                    break;
+                }
+                i++;
+            }
 
             // end
 
@@ -82,7 +186,25 @@ namespace Lab2
             double SS = 0;
             double SY = 0;
 
+
             // code here
+            for (double x = a; x <= b + E; x += h)
+            {
+                SY += Math.Atan(x);
+                int sign = -1;
+                double mn = 1;
+                for (int i = 0; ; i++)
+                {
+                    double s = -1 * sign * x * mn / (2 * i + 1);
+                    SS += s;
+                    sign *= -1;
+                    mn *= x * x;
+                    if (Math.Abs(s) < E)
+                    {
+                        break;
+                    }
+                }
+            }
 
             // end
 
