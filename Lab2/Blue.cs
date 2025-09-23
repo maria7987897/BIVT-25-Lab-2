b00b5d02d8e800f602b1a49273b51ed9fa455b52
Илья Киселёв <138257294+nakakorik@users.noreply.github.com>
2025-09-23 12:10:40 +0200

    7-8

--- Lab2/Blue.cs
+++ Lab2/Blue.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq.Expressions;
 using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Lab2
 {
@@ -10,13 +12,13 @@ namespace Lab2
         public double Task1(int n, double x)
         {
             double answer = 0;
-
             // code here
 
             // end
 
             return answer;
         }
+
         public double Task2(int n)
         {
             double answer = 0;
@@ -74,9 +76,30 @@ namespace Lab2
             int c = 0;
 
             // code here
-
+            double p = S;
+            a += p;
+            for (int i = 0; i<6; i++)
+            {
+                p = p + (I / 100) * p;
+                a += p;
+            }
+            p = S;
+            double sm = 0;
+            while (sm < 100)
+            {
+                sm += p;
+                b++;
+                p = p + (I / 100) * p;
+            }
+            p = S;
+            while (p < 42)
+            {
+                p = p + (I / 100) * p;
+                c++;
+            }
             // end
-
+            
+        
             return (a, b, c);
         }
         public (double SS, double SY) Task8(double a, double b, double h)
@@ -85,7 +108,30 @@ namespace Lab2
             double SY = 0;
 
             // code here
+            double sm = 0;
+            for (double x = a; x <= b + 0.0001; x+=h)
+            {
+                Console.WriteLine(x);
+                sm = 0;
+                double x_pow = 1;
+                double i_fact = 1;
+                int i = 0;
+                double el = ( (2 * i + 1) * x_pow )/ (i_fact);
+                while (Math.Abs(el) > 0.0001)
+                {
 
+                    el = ((2 * i + 1) * x_pow) / (i_fact);
+                    sm += el;
+
+                    i++;
+                    x_pow *= x * x;
+                    i_fact *= i;
+
+                }
+                SS += sm;
+                SY += (1 + 2 * x * x) * Math.Pow(Math.E, x * x);
+            }
+            Console.WriteLine((SS, SY));
             // end
 
             return (SS, SY);
