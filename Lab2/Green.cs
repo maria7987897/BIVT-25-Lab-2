c747af740f1543f04d38ef98de09beb25686fb44
MKotikova <m.kotikova22@gmail.com>
2025-09-23 11:11:09 +0300

    Update Green.cs

--- Lab2/Green.cs
+++ Lab2/Green.cs
@@ -6,22 +6,50 @@ namespace Lab2
     {
         const double E = 0.0001;
         const double Da = 0.0000000001;
+        public long fac(int n) //метод нахождения факториала
+        {
+            long k = 1;
+            for (int t = 1; t <= n; t++)
+            {
+                k = k * t;
+            }
+            return k;
+        }
+        public int power(int x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return power(x, n-1) * x;
+            }
+        }
         public double Task1(int n)
         {
             double answer = 0;
-
             // code here
-
-            // end
-
-            return answer;
+            double a = 0;
+            for (int i = 2; i <= n; i += 2)
+            {
+                a = i;
+                a = a / (i + 1);
+                answer += a;
+                // end
+            }
+                return answer;
         }
         public double Task2(int n, double x)
         {
             double answer = 0;
 
             // code here
-
+            answer++;
+            for (int i = 1; i <= n; i++)
+            {
+                answer += Math.Pow(x, -i);
+            }
             // end
 
             return answer;
@@ -31,29 +59,52 @@ namespace Lab2
             long answer = 0;
 
             // code here
-
-            // end
-
-            return answer;
+            for (int i = 1; i <= n; i++)
+            {
+                long k = 1;
+                k = fac(i);
+                answer += k;
+                // end
+            }
+            answer += 1; //т.к. 0! = 1
+                return answer;
         }
         public double Task4(double x)
         {
             double answer = 0;
-
             // code here
-
+            double a = 1;
+            int n = 1;
+            while (Math.Abs(a) > E)
+            {
+                a = Math.Sin(n * Math.Pow(x, n));
+                answer += a;
+                n++;
+            }
             // end
-
             return answer;
         }
         public int Task5(double x)
         {
             int answer = 0;
-
+            int n = 0;
             // code here
-
+            if (x > 0)
+            {
+                while (Math.Abs(1 / Math.Pow(x, (n - 1))) - Math.Abs(1 / Math.Pow(x, n)) > 0.0001)
+                {
+                    n++;
+                }
+            }
+            else
+            {
+                while (Math.Abs(1 / Math.Pow(x, n)) + Math.Abs(1 / Math.Pow(x, (n - 1))) > 0.0001)
+                {
+                    n++;
+                }
+            }
             // end
-
+            answer = n;
             return answer;
         }
         public int Task6(int limit)
@@ -61,7 +112,13 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            int elem = 1, i = 0;
+            while (elem < limit)
+            {
+                elem *= 2;
+                answer += elem;
+                i++;
+            }
             // end
 
             return answer;
@@ -72,7 +129,11 @@ namespace Lab2
             int answer = 0;
 
             // code here
-
+            while (L > Da)
+            {
+                L /= 2;
+                answer++;
+            }
             // end
 
             return answer;
@@ -83,7 +144,23 @@ namespace Lab2
             double SY = 0;
 
             // code here
+            for (double x = a; x <= b + 0.0001; x += h)
+            {
+                int p = -1;
+                double n = 1;
+                double sn = 1;
+                int i = 0;
+                while (Math.Abs(sn) >= E)
+                {
+                    sn = -p * x * n / (2 * i + 1);
+                    p = -1 * p;
+                    SS += sn;
+                    n = n * x * x;
+                    i++;
+                }
 
+                SY += Math.Atan(x);
+            }
             // end
 
             return (SS, SY);
