633207a6bc9a7fa28033772a1789b3c7b95cc787
Евгений Егоров <103120931+LeHachek@users.noreply.github.com>
2025-09-25 10:23:09 +0300

    Add files via upload

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -85,7 +85,42 @@ namespace Lab2
             double SY = 0;
 
             // code here
-
+            double MyPow(double x, int n)
+            {
+                double S = 1;
+                for (int i = 0; i < n; i++)
+                {
+                    S *= x;
+                }
+                return S;
+            }
+            int Fact(int x)
+            {
+                int S = 1;
+                for (int i = 1; i <= x; i++)
+                {
+                    S *= i;
+                }
+                return S;
+            }
+            for (double x = a; x <= b; x += h)
+            {
+                x = Math.Round(x, 5);
+                double S=0;
+                int i = 0;
+                while (true)
+                {
+                    double el = MyPow(-1, i) * MyPow(x,2*i)/Fact(2*i);
+                    S += el;
+                    if (Math.Abs(el) < 0.0001)
+                        break;
+                    i++;
+                }
+                SS += S;
+                SY += Math.Cos(x);
+            }
+            SS = Math.Round(SS, 6);
+            SY = Math.Round(SY, 6);
             // end
 
             return (SS, SY);
